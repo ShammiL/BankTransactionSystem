7b874486a2f1f10a4beca5c7e550273f29e80d9d
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-12-12 01:00:40 +0530

    create database functions and procedures in NOdeJS

---
+++ Core/databaseEvents/procedures/functions.js
@@ -0,0 +1,46 @@
+Db = require('../../DB')
+eventNames = require('../eventNames/eventNames')
+
+var connection = Db.mysqlConnection
+// var procedureName = eventNames.checkRemainingLoanAmount;
+
+async function checkRemainingLoanAmount(data) {
+    console.log("select " + eventNames.checkRemainingLoanAmount + "(" + data + ")")
+    // console.log("Pro", data)
+    const result = await connection.query("select " + eventNames.checkRemainingLoanAmount + "(" + data + ")");
+    if (!result.length)
+        throw new Errors.NotFound('Error');
+    console.log("RES", Object.values(result[0][0])[0]);
+    return Object.values(result[0][0])[0]
+}
+// checkRemainingLoanAmount('1234');
+
+async function checkFDInstallment(data) {
+    console.log("select " + eventNames.checkFDInstallment + "(" + data + ")")
+    // console.log("Pro", data)
+    const result = await connection.query("select " + eventNames.checkFDInstallment + "(" + data + ")");
+    if (!result.length)
+        throw new Errors.NotFound('Error');
+    console.log("RES", Object.values(result[0][0])[0]);
+    return Object.values(result[0][0])[0]
+}
+// checkFDInstallment('"66b1811b-f"');
+// checkFDInstallment('"b192b124-4"');
+// // checkFDInstallment('"45880c14-a"');
+// checkFDInstallment('"45880c4-a"');
+
+async function checkForonlineLoan(data) {
+    console.log("select " + eventNames.checkForonlineLoan + "(" + data + ")");
+    const result = await connection.query("select " + eventNames.checkForonlineLoan + "(" + data + ")");
+    if (!result.length)
+        throw new Errors.NotFound('Error');
+    console.log("RES", Object.values(result[0][0])[0]);
+    return Object.values(result[0][0])[0]
+}
+// checkForonlineLoan("'45880c14-a',6000");
+// checkForonlineLoan("'b192b124-4',119");
+// checkForonlineLoan("'66b1811b-f',600");
+
+module.exports.checkForonlineLoan = checkForonlineLoan;
+module.exports.checkRemainingLoanAmount = checkRemainingLoanAmount;
+module.exports.checkFDInstallment = checkFDInstallment;
--- Core/databaseEvents/procedures/procedures.js
+++ Core/databaseEvents/procedures/procedures.js
@@ -34,6 +34,83 @@ async function managerRegisterProcedure(data) {
     console.log("RES", result)
     return result
 }
+async function createAccountCustomer(data) {
+    console.log("call " + eventNames.accountForCustomer + "(" + data + ")")
+    // console.log("Pro", data)
+    const result = await connection.query("call " + eventNames.accountForCustomer + "(" + data + ")");
+    if (!result.length)
+        throw new Errors.NotFound('Error');
+    console.log("RES", result)
+    return result
+}
+// createAccountCustomer("'27', 'checking', 'child', '66b1811b-f','',''")
+async function addOnlineLoan(data) {
+    console.log("call " + eventNames.addOnlineLoan + "(" + data + ")")
+    // console.log("Pro", data)
+    const result = await connection.query("call " + eventNames.addOnlineLoan + "(" + data + ")");
+    if (!result.length)
+        throw new Errors.NotFound('Error');
+    console.log("RES", result)
+    return result
+}
+async function approveLoan(data) {
+    console.log("call " + eventNames.approveLoan + "(" + data + ")")
+    // console.log("Pro", data)
+    const result = await connection.query("call " + eventNames.approveLoan + "(" + data + ")");
+    if (!result.length)
+        throw new Errors.NotFound('Error');
+    console.log("RES", result)
+    return result
+}
+async function createFDaccount(data) {
+    console.log("call " + eventNames.createFDaccount + "(" + data + ")")
+    // console.log("Pro", data)
+    const result = await connection.query("call " + eventNames.createFDaccount + "(" + data + ")");
+    if (!result.length)
+        throw new Errors.NotFound('Error');
+    console.log("RES", result)
+    return result
+}
+
+
+async function makeOfflineDeposite(data) {
+    console.log("call " + eventNames.makeOfflineDeposite + "(" + data + ")")
+    // console.log("Pro", data)
+    const result = await connection.query("call " + eventNames.makeOfflineDeposite + "(" + data + ")");
+    if (!result.length)
+        throw new Errors.NotFound('Error');
+    console.log("RES", result)
+    return result
+}
+async function onlineTransfer(data) {
+    console.log("call " + eventNames.onlineTransfer + "(" + data + ")")
+    // console.log("Pro", data)
+    const result = await connection.query("call " + eventNames.onlineTransfer + "(" + data + ")");
+    if (!result.length)
+        throw new Errors.NotFound('Error');
+    console.log("RES", result)
+    return result
+}
+
+async function withdrawalAccount(data) {
+    console.log("call " + eventNames.withdrawalAccount + "(" + data + ")")
+    // console.log("Pro", data)
+    const result = await connection.query("call " + eventNames.withdrawalAccount + "(" + data + ")");
+    if (!result.length)
+        throw new Errors.NotFound('Error');
+    console.log("RES", result)
+    return result
+}
+// withdrawalAccount("'9989',170024 ,'9', '','' ,170024")
+
+
 module.exports.managerRegisterProcedure = managerRegisterProcedure;
 module.exports.individualCustomerLogin = individualCustomerLogin;
 module.exports.companycustomerLogin = companycustomerLogin;
+module.exports.createAccountCustomer = createAccountCustomer
+module.exports.addOnlineLoan = addOnlineLoan;
+module.exports.approveLoan = approveLoan;
+module.exports.createFDaccount = createFDaccount;
+module.exports.makeOfflineDeposite = makeOfflineDeposite;
+module.exports.onlineTransfer = onlineTransfer;
+module.exports.withdrawalAccount = withdrawalAccount;
