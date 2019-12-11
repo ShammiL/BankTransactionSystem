4f8b5f20ea2ea5fd6d2f1571cafe6f700dce72bf
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-12-11 20:27:08 +0530

    config part of database

--- Core/databaseEvents/eventNames/eventNames.js
+++ Core/databaseEvents/eventNames/eventNames.js
@@ -2,9 +2,28 @@
 exports.individualcustomerLogin = 'individualcustomerLogin'
 exports.companycustomerLogin = 'companycustomerLogin'
 exports.managerRegister = 'managerRegister'
+exports.accountForCustomer = 'accountForCustomer'
+exports.addOnlineLoan = 'addOnlineLoan'
+exports.approveLoan = 'approveLoan'
+exports.createFDaccount = 'createFDaccount'
+exports.makeOfflineDeposite = 'makeOfflineDeposite'
+exports.onlineTransfer = 'onlineTransfer'
+exports.withdrawalAccount = 'withdrawalAccount'
 
 //views
 
 exports.individualCustomerview = 'getalldataofindividualcustomer'
 exports.companyCustomerview = 'getalldataofcompanycustomer'
 exports.managerview = 'getalldataofmanager'
+exports.allcheckingaccountofacustomerHide = 'allcheckingaccountofacustomerHide'
+exports.allsavingsaccountofacustomer = 'allsavingsaccountofacustomer'
+exports.depositereceiptsHide = 'depositereceiptsHide'
+exports.transferreceiptsHide = 'transferreceiptsHide'
+
+//funcs
+
+exports.checkFDInstallment = 'checkFDInstallment'
+exports.checkForonlineLoan = 'checkForonlineLoan'
+exports.checkRemainingLoanAmount = 'checkRemainingLoanAmount'
+
+//triggers
