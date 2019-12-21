6250c285db7e4dc614a75eea8170b38b135ba80a
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-12-21 13:55:11 +0530

    search bar and loan view of manager

--- View/viewbanktransactionsystem/src/components/EmployeeComponents/ViewLoan.js
+++ View/viewbanktransactionsystem/src/components/EmployeeComponents/ViewLoan.js
@@ -2,19 +2,20 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 // import { Link } from 'react-router-dom'
 import { typeDetails } from '../../actions/searchbarAction'
+import SearchBar from './../otherComponents/SearchBar'
 
 export class ViewLoan extends Component {
 
-    submit = e => {
-        e.preventDefault();
-        // console.log("Withdrawal Types", { username: this.props.accountNum, password: this.props.amount })
-        // this.props.fetchLoggedUser(this.props.username, this.props.password)
-        // localStorage.setItem('usertoken', res.data)
-    }
-    change = e => {
-        this.props.typeDetails(e.target.name, e.target.value); //connect connect this prop
+    // submit = e => {
+    //     e.preventDefault();
+    //     // console.log("Withdrawal Types", { username: this.props.accountNum, password: this.props.amount })
+    //     // this.props.fetchLoggedUser(this.props.username, this.props.password)
+    //     // localStorage.setItem('usertoken', res.data)
+    // }
+    // change = e => {
+    //     this.props.typeDetails(e.target.name, e.target.value); //connect connect this prop
 
-    }
+    // }
 
 
     constructor(props) {
@@ -24,21 +25,19 @@ export class ViewLoan extends Component {
     render() {
         return (
             <div>
-                <form onSubmit={this.submit}>
-                    <h5>Search </h5>
-                    <input onChange={this.change} type="text" name="content" placeholder="Search" />
-                    <button type="submit"> Search </button>
-                </form>
+                <SearchBar
+                    type="viewAllLoan"
+                />
             </div>
         )
     }
 }
 
 const mapStatesToProps = state => ({
-    content: state.searchbar.content
+    // content: state.searchbar.content
 })
 const mapActionToProps = {
-    typeDetails: typeDetails,
+    // typeDetails: typeDetails,
 }
 
 export default connect(mapStatesToProps, mapActionToProps)(ViewLoan)
