26802bf35bcb54439036c4c6c00de12c4f55fd33
Shammi <shammi0107@gmail.com>
2019-12-27 12:47:24 +0530

    refactor account close

--- Models/allAccountModel/allAccountController.js
+++ Models/allAccountModel/allAccountController.js
@@ -2,9 +2,24 @@
 allAccountModel = require("./allAccountModel")
 
 exports.close = (req, res) => {
-    allAccountModel.close(req.body.id).then((result) => {
-        res.send(result)
-    });
+    allAccountModel.getByID(req.body.id).then((result) => {
+        if(result[0].closed == 1)
+        {
+            res.send({
+                "success":"Account already closed",
+                "code":204
+            })
+        }
+        else{
+            allAccountModel.close(req.body.id).then((result) => {
+                res.send({
+                    "code":200,
+                    "result":result
+                })
+            });
+        }   
+        
+        });
 }
 exports.reopen = (req, res) => {
     allAccountModel.getByID(req.body.id).then((result) => {
