350e2dc7a7a0fe58bf61dbe01d8e3aed5472bb8f
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-12-30 20:20:22 +0530

    refactor database user

--- Config/config.js
+++ Config/config.js
@@ -1,8 +1,10 @@
 
 //database part
 module.exports.databaseName = "bank_transaction_system";
-module.exports.databaseUser = "root";
-module.exports.databasePassword = "";
+// module.exports.databaseUser = "root";
+// module.exports.databasePassword = "";
+module.exports.databaseUser = "bankadmin";
+module.exports.databasePassword = "ssyd@UoM2019";
 module.exports.host = "localhost";
 
 // envpart
