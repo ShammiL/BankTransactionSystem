946a39ee4b5e95059d20f1eedeffd62f5a93300a
Shammi <shammi0107@gmail.com>
2020-05-07 08:14:27 +0530

    Update config.js

--- Config/config.js
+++ Config/config.js
@@ -3,8 +3,8 @@
 module.exports.databaseName = "bank_transaction_system";
 // module.exports.databaseUser = "root";
 // module.exports.databasePassword = "";
-module.exports.databaseUser = "bankadmin";
-module.exports.databasePassword = "ssyd@UoM2019";
+module.exports.databaseUser = "root";
+module.exports.databasePassword = "";
 module.exports.host = "localhost";
 
 // envpart
